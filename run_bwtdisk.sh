cc6705222cce388319652bfee9dcaade7eb92d61
Karel Břinda <karel.brinda@gmail.com>
2025-01-17 11:11:27 +0100

    Adjust paths in run_bwtdisk.sh

--- run_bwtdisk.sh
+++ run_bwtdisk.sh
@@ -6,10 +6,10 @@ set -eu
 ./bwtdisk-prepare $1 > $1.joined
 
 # Reverse the text, because bwtdisk constructs the BWT of the reverse text by default.
-text_rev -vvv $1.joined $1.joined.rev
+./bwtdisk/text_rev -vvv $1.joined $1.joined.rev
 
 # Generate the BWT.
-bwte -vvv $1.joined.rev
+./bwtdisk/bwte -vvv $1.joined.rev
 
 # Rename the bwt
 mv $1.joined.rev.bwt `basename $1 .fa`.bwtdisk
